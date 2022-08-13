1919ce1c66e6b63ae7362e616704687261bd5ea1
AybYrn <aybukeyaren--@outlook.com>
2022-08-13 21:33:15 +0300

    React Fetch and Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,14 +3,27 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Button } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
+import React from "react";
 
 function App() {
+  const [count, setCount] = React.useState(0);
+
+  function handleClick() {
+    setCount((prevData) => {
+      return prevData + 1;
+    });
+  }
+  console.log("component rendered");
+  React.useEffect(function () {
+    console.log("useEffect rendered");
+  });
+
   return (
     <div className="App">
       <Container>
         <Card>
-          <h1 className="header">Welcome To React-Bootstrap</h1>
-          <Button>COUNT</Button>
+          <h3 className="header">The Count is : {count} </h3>
+          <Button onClick={handleClick}>COUNT</Button>
         </Card>
       </Container>
     </div>
