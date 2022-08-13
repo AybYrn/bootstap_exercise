a7aed57fdb370d5e11bef534f81fc676dc632b31
AybYrn <aybukeyaren--@outlook.com>
2022-08-13 21:16:17 +0300

    React Fetch and Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Card>
+          <h1 className="header">Welcome To React-Bootstrap</h1>
+          <Button>COUNT</Button>
+        </Card>
+      </Container>
     </div>
   );
 }
